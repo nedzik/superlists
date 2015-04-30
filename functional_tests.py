28a3d2bd71a356062fa26a77ce75522c7677cf81
Sergey Nedzelskyi <sergey.nedzelskyi@sabre.com>
2015-04-30 15:09:40 -0500

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -6,7 +6,6 @@ from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
-
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -29,9 +28,11 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        does_row_exist = any(row.text == '1: Buy peacock feathers' for row in rows)
+        self.assertTrue(does_row_exist, "New to-do item did not appear in table")
 
         self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
